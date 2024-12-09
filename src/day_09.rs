3730dbd27c409d673c4a2fafa621939a5b6519bd
Gernot Raudner <office@gernotraudner.at>
2024-12-09 19:50:31 +0000

    day 09 complete

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,8 +1,20 @@
-use std::fs;
+use std::{collections::VecDeque, fs, vec};
 
+#[derive(Clone, Debug)]
 struct DiskSpace {
-    index: usize,
+    size: usize,
+    file_id: Option<usize>,
+    start: usize
+}
 
+impl DiskSpace {
+    fn new(size: usize, file_id: Option<usize>, start: usize) -> DiskSpace {
+        DiskSpace {
+            size, 
+            file_id,
+            start
+        }
+    }
 }
 
 // Main function
@@ -15,31 +27,75 @@ pub fn day_09() {
 
 // Puzzle 1 function
 fn puzzle1(input_file: &str) -> usize {
-    let mut entries: Vec<Option<usize>> = fs::read_to_string(input_file)
+    let mut entries: VecDeque<Option<usize>> = fs::read_to_string(input_file)
         .expect("Failed to read input file")
         .chars()
         .map(|char| char.to_digit(10).expect("Expecting all chars to be digits") as usize)
         .enumerate()
         .flat_map(|(file_id, count)| {
             if file_id % 2 == 0 {
-                vec![Some(file_id); count]
+                vec![Some(file_id / 2); count]
             } else {
                 vec![None; count]
             }
         })
         .collect();
-    let populated = vec![];
+    let mut populated = vec![];
     
-    
+    while let Some(entry) = entries.pop_front() {
+        if let Some(file_id) = entry {
+            populated.push(file_id);
+        } else {
+            // pop until we found `Some`
+            while let Some(last_entry) = entries.pop_back() {
+                if let Some(last_entry_file_id) = last_entry {
+                    populated.push(last_entry_file_id);
+                    break;
+                }
+            }  
+        }
+    }
 
-
-    1928
+    populated
+        .iter()
+        .enumerate()
+        .map(|(index, file_id)| index * file_id)
+        .sum()
 }
 
 // Puzzle 2 function
 fn puzzle2(input_file: &str) -> usize {
-    let input = fs::read_to_string(input_file).expect("Failed to read input file");
-    0
+    let (_, mut entries, mut empty_spaces) = fs::read_to_string(input_file)
+        .expect("Failed to read input file")
+        .chars()
+        .map(|char| char.to_digit(10).expect("Expecting all chars to be digits") as usize)
+        .enumerate()
+        .fold((0, vec![], vec![]), |(index, mut entries, mut empty_spaces), (start, size)| {
+            let file_id = start / 2;
+            if start % 2 == 0 {
+                entries.push(DiskSpace::new(size, Some(file_id), index));
+            } else {
+                empty_spaces.push(DiskSpace::new(size, None, index));
+            }
+            (index + size, entries, empty_spaces)
+        });
+    
+
+    for entry in entries.iter_mut().rev() {
+        if let Some(empty_space) = empty_spaces
+            .iter_mut()
+            .find(|space| space.size >= entry.size && space.start < entry.start)
+        {
+            entry.start = empty_space.start;
+            empty_space.size -= entry.size;
+            empty_space.start += entry.size;
+        }
+    }
+    entries
+        .iter()
+        .flat_map(|entry| (0..entry.size)
+            .map(|id| (entry.start + id) * entry.file_id.unwrap()))
+        .sum()
 }
 
 #[cfg(test)]
@@ -53,6 +109,6 @@ mod tests {
 
     #[test]
     fn test_puzzle2() {
-        assert_eq!(puzzle2("./input_test/day_09.txt"), 0);
+        assert_eq!(puzzle2("./input_test/day_09.txt"), 2858);
     }
 }
